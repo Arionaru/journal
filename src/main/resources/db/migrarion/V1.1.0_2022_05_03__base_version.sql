b87d2be34a3ae82a72e3a8537180f25fb72a451e
Ariona <e.petrushenko@inbox.ru>
2022-05-04 15:37:54 +0300

    init

---
+++ src/main/resources/db/migrarion/V1.1.0_2022_05_03__base_version.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS event (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    source TEXT NOT NULL,
+    source_id TEXT NOT NULL,
+    description jsonb,
+    action TEXT NOT NULL,
+    username TEXT NOT NULL,
+    status TEXT NOT NULL,
+    result TEXT,
+    event_time TIMESTAMP NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS journal_source_source_id on event(source, source_id);
